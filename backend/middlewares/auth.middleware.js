4a33a1674b51a570a5c204e488f7cf9846ba4b0b
Vladimir <vladimirkrylov@gmail.com>
2022-02-03 11:39:51 +0300

    fix: -Исправлен путь на фронтенде к Api -Исправлен баг при регистрации на бекенде

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,17 +3,17 @@ const TOKEN_SECRET = require('../utils/secret');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 function authorize(req, res, next) {
-  const token = req.cookies.jwt;
-  if (!token) {
-    return next(new UnauthorizedError('Необходима авторизация'));
-  }
+	const token = req.cookies.jwt;
+	if (!token) {
+		return next(new UnauthorizedError('Необходима авторизация!'));
+	}
 
-  try {
-    req.user = jwt.verify(token, TOKEN_SECRET);
-  } catch (e) {
-    return next(new UnauthorizedError('Необходима авторизация'));
-  }
-  return next();
+	try {
+		req.user = jwt.verify(token, TOKEN_SECRET);
+	} catch (e) {
+		return next(new UnauthorizedError('Необходима авторизация!!'));
+	}
+	return next();
 }
 
 module.exports = { authorize };
--- frontend/src/utils/MestoAuth.js
+++ frontend/src/utils/MestoAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.krylov.students.nomoredomains.work/";
 
 const responseCheck = (response) =>
   response.ok ? response.json() : Promise.reject(`Ошибка ${response.status}`);
