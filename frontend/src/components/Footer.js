fd51fd98e301a6851b29f0556948254124390cd6
Vladimir <vladimirkrylov@gmail.com>
2022-02-15 10:27:41 +0300

    fix

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer page__item">
-            <p className="footer__copy">&copy; 2022 Mesto Russia</p>
-        </footer>
-    );
-}
-
-export default Footer;
